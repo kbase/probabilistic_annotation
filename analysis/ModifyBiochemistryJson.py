702fdb3effac40beb978bedc57eecacfd22e4e71
Matthew Benedict <mattb112885@autorecon.(none)>
2013-01-07 19:50:18 -0600

    Making sure latest version is in the repo

--- analysis/ModifyBiochemistryJson.py
+++ analysis/ModifyBiochemistryJson.py
@@ -74,25 +74,75 @@ def addRxnProbabilitiesToBiochemistryJson(reaction_probability_file, biochemistr
     
     # Now we need to iterate over all of the reactions and add the appropriate probabilities
     # to each of these.
-
     rxnList = resp["reactions"]
     for ii in range(len(rxnList)):
         myuuid = rxnList[ii]["uuid"]
-        # -5 means database issue \ not found.
-        myProbability = -5
+        myProbability = 0
         myComplexInfo = ""
         myGPR = ""
+        # These flags indicate database issues that could bias the probabilities.
+        #
+        # If all of them are FALSE or if only reactionHasComplex is true, the probability will be 0
+        # but it is due to missing data.
+        #
+        # If only allComplexesHaveRepresentativeRoles is false, that only means there is some missing data
+        # but still enough to test presence of some subunits of some complexes.
+        reactionHasComplex = False
+        oneComplexHasRepresentativeRoles = False
+        allComplexesHaveRepresentativeRoles = False
+        # If the database versions are consistent this should always be the case
         if myuuid in uuidToModelId:
             modelId = uuidToModelId[myuuid]
             if modelId in modelToKbase:
                 kbaseId = modelToKbase[modelId]
+                # This one is only the case if there are complexes associated with the reaction
                 if kbaseId in kbaseIdToInfo:
+                    reactionHasComplex = True
+                    ''' There are three possibilities for each complex.
+                    1: The roles attached to the complex had no representatives in our BLAST db (BAD)
+                    2: The roles attached to the complex had representatives, but they were not found in
+                    our BLAST search (OK)
+                    3: The roles attached were all found with some probability in the BLAST search (OK)
+
+                    Since there are multiple possibilities for complexes we need to decide when we should
+                    call it OK and when we can't.
+
+                    For now I will set separate flags for the occasion when one complex has roles and one doesn't
+                    (and the calculated probability is for the complex with a probability)
+                    and the occasion when NONE of the complexes have representeatives of their roles
+                    (and the calculated probability is artificially 0)
+
+                    PARTIAL cases are treated as "has representatives" for this purpose.
+                    Therefore, if only allComplexesHaveRepresentativeRoles is false, that means
+                    there is incomplete information, but at least one subunit of one complex attached
+                    to the reaction had a representative that we could use to calculate a probability.
+
+                    '''
+                    # CPLX_FULL   [ok]
+                    # CPLX_NOTTHERE [ok]
+                    # CPLX_PARTIAL [sort of ok - treated as OK for this purpose]
+                    # CPLX_NOREPS [bad]
                     myProbability = kbaseIdToInfo[kbaseId][0]
-                    myComplexInfo = kbaseIdToInfo[kbaseId][1]
                     myGPR = kbaseIdToInfo[kbaseId][2]
+                    myComplexInfo = kbaseIdToInfo[kbaseId][1]
+                    if "CPLX_NOREPS" in myComplexInfo:
+                        if "CPLX_FULL" in myComplexInfo or "CPLX_NOTTHERE" in myComplexInfo or "CPLX_PARTIAL" in myComplexInfo:
+                            oneComplexHasRepresentativeRoles = True
+                        else:
+                            # No complexes have representative roles.
+                            pass
+                    else:
+                        # All of them are either CPLX_FULL or CPLX_NOTTHERE
+                        oneComplexHasRepresentativeRoles = True
+                        allComplexesHaveRepresentativeRoles = True
+
         resp["reactions"][ii]["probability"] = myProbability
         resp["reactions"][ii]["complexinfo"] = myComplexInfo
         resp["reactions"][ii]["GPR"]         = myGPR
+        resp["reactions"][ii]["reactionHasComplex"]                   = reactionHasComplex
+        resp["reactions"][ii]["oneComplexHasRepresentativeRoles"]     = oneComplexHasRepresentativeRoles
+        resp["reactions"][ii]["allComplexesHaveRepresentativeRoles"]  = allComplexesHaveRepresentativeRoles
+        
     json.dump(resp, open(output_file, "w"), indent=4)
 
 if __name__ == "__main__":
