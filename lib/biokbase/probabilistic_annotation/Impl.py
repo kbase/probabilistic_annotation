25b8cfe7fa4f04d6c9ca521ac6b4f375de01c18e
Mike Mundy <mundy.michael@mayo.edu>
2013-09-05 09:43:05 -0500

    Updated documentation, javascript client

--- lib/biokbase/probabilistic_annotation/Impl.py
+++ lib/biokbase/probabilistic_annotation/Impl.py
@@ -49,12 +49,14 @@ Module Description:
 The purpose of the Probabilistic Annotation service is to provide users with
 alternative annotations for genes, each attached to a likelihood score, and to
 translate these likelihood scores into likelihood scores for the existence of
-reactions in metabolic models.
+reactions in metabolic models.  With the Probabilistic Annotation service:
 
-- Allows users to quickly assess the quality of an annotation.
+- Users can quickly assess the quality of an annotation.
+
 - Reaction likelihood computations allow users to estimate the quality of
   metabolic networks generated using the automated reconstruction tools in
   other services.
+
 - Combining reaction likelihoods with gapfilling both directly incorporates
   available genetic evidence into the gapfilling process and provides putative
   gene annotations automatically, reducing the effort needed to search for
