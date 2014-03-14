8032601159e3d9e7d3f278c142866c685052525e
Mike Mundy <mundy.michael@mayo.edu>
2014-03-14 10:30:52 -0500

    convert pa-url to python, use kbase_config file for saving url

--- lib/biokbase/probabilistic_annotation/Helpers.py
+++ lib/biokbase/probabilistic_annotation/Helpers.py
@@ -3,6 +3,45 @@
 import os
 import sys
 import time
+from biokbase.probabilistic_annotation.DataParser import getConfig, readConfig
+from biokbase.auth import kb_config
+from ConfigParser import ConfigParser
+
+DefaultURL = 'https://kbase.us/services/probabilistic_annotation/'
+
+''' Get the current URL for the service. '''
+
+def get_url():
+    # Just return the default URL when running in IRIS.
+    if 'KB_RUNNING_IN_IRIS' in os.environ:
+        return DefaultURL
+
+    # Get the URL from the config file or use the default if it is not set.
+    config = getConfig(kb_config)
+    if 'url' in config:
+        currentURL = config['url']
+    else:
+        currentURL = DefaultURL;
+    return currentURL
+
+''' Set the current URL for the service and store in config file. '''
+
+def set_url(newURL):
+    # Check for special value for the default URL.
+    if newURL ==  'default':
+        newURL = DefaultURL
+
+    # Just return the URL when running in IRIS. There is no place to save it.
+    if 'KB_RUNNING_IN_IRIS' in os.environ:
+        return newURL
+
+    # Save the new URL to the config file.
+    config = readConfig(kb_config)
+    config.set('probabilistic_annotation', 'url', newURL)
+    with open(kb_config, 'w') as configfile:
+        config.write(configfile)
+    return newURL
+
 
 ''' Get a timestamp in the format required by user and job state service.
     Add deltaSeconds to the current time to get a time in the future. '''
