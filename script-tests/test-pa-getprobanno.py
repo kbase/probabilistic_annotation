3c60c3504d373104a46505ec53d64a386ff310c1
Mike Mundy <mundy.michael@mayo.edu>
2014-08-20 10:48:42 -0500

    add tests for pa-getprobanno and pa-getrxnprobs commands

---
+++ script-tests/test-pa-getprobanno.py
@@ -0,0 +1,59 @@
+import sys
+import unittest
+import subprocess
+import time
+import os
+
+class TestGetProbannoScript(unittest.TestCase):
+    '''
+    Test inputs and option processing
+    '''
+        
+    def setUp(self):
+        self.cmd = os.path.join(os.environ["KB_TOP"], "bin/pa-getprobanno")
+
+    def test_help(self):
+        '''Run pa-getprobanno --help and verify that the major sections in the help text are present'''
+        
+        args = [ self.cmd, "--help" ]
+        proc = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        (so, se) = proc.communicate()
+        self.assertEqual(proc.returncode, 0)
+        self.assertNotEqual(so.find("NAME"), -1)
+        self.assertNotEqual(so.find("SYNOPSIS"), -1)
+        self.assertNotEqual(so.find("DESCRIPTION"), -1)
+        self.assertNotEqual(so.find("EXAMPLES"), -1)
+        self.assertEqual(se, '')
+                
+    def test_badOption(self):
+        '''Run pa-getprobanno with a bad option and verify that the error message is returned.'''
+        
+        args = [ self.cmd, "kb|g.8622.probanno", "--chia" ]
+        proc = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        (so, se) = proc.communicate()
+        self.assertNotEqual(proc.returncode, 0)
+        self.assertEqual(so, '')
+        self.assertNotEqual(se.find("unrecognized arguments:"), -1)
+        
+    def test_missingOptionValue(self):
+        '''Run pa-getprobanno with a missing option value and verify that the error message is returned.'''
+        
+        args = [ self.cmd, "kb|g.8622.probanno", "--workspace" ]
+        proc = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        (so, se) = proc.communicate()
+        self.assertNotEqual(proc.returncode, 0)
+        self.assertEqual(so, '')
+        self.assertNotEqual(se.find("expected one argument"), -1)
+        
+    def test_missingArg(self):
+        '''Run pa-getprobanno with a missing argument and verify that the error message is returned.'''
+        
+        args = [ self.cmd, "--workspace", "ProbAnnoTest" ]
+        proc = subprocess.Popen(args, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        (so, se) = proc.communicate()
+        self.assertEqual(proc.returncode, 2)
+        self.assertEqual(so, '')
+        self.assertNotEqual(se.find("too few arguments"), -1)
+
+if __name__ == '__main__':
+    unittest.main()
